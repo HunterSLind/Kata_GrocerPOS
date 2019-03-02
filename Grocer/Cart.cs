e456ff0d803d39d58517178fbc53115f24c6d7d0
HLind <hunterslind@gmail.com>
2019-03-02 15:51:52 -0600

    Renamed argument for method Cart.AddItem Argument was: units Argument is now: amount

--- Grocer/Cart.cs
+++ Grocer/Cart.cs
@@ -7,14 +7,14 @@ namespace Grocer
     {
         public Dictionary<InventoryItem, decimal> CartItems = new Dictionary<InventoryItem, decimal>();
 
-        public void AddItem(string item, decimal units = 1)
+        public void AddItem(string item, decimal amount = 1)
         {
             InventoryItem itemToAdd = ClientInventory.InventoryDictionary[item];
             if (!CartItems.ContainsKey(itemToAdd))
             {
                 CartItems.Add(itemToAdd, 0);
             }
-            CartItems[itemToAdd] += units;
+            CartItems[itemToAdd] += amount;
         }
     }
 }
