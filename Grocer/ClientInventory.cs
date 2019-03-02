82cd87e548d1c3591185308ad24881c97f0f3fd8
HLind <hunterslind@gmail.com>
2019-03-02 17:08:16 -0600

    Refactored out InventoryVariables into its own file.

--- Grocer/ClientInventory.cs
+++ Grocer/ClientInventory.cs
@@ -16,25 +16,5 @@ namespace Grocer
 
     }
 
-    public static class InventoryVariables
-    {
-        public static string HAMBURGER_ID = "hamburger";
-        public static decimal HAMBURGER_PRICE = 4.99m;
-        public static decimal HAMBURGER_MARKDOWN = 0;
 
-        public static InventoryItem HAMBURGER = new InventoryItem
-        {
-            Name = InventoryVariables.HAMBURGER_ID,
-            Price = InventoryVariables.HAMBURGER_PRICE,
-            MarkDown = InventoryVariables.HAMBURGER_MARKDOWN
-        };
-
-        public static void ResetInventory()
-        {
-            ClientInventory.InventoryDictionary = new Dictionary<string, InventoryItem>
-            {
-                {InventoryVariables.HAMBURGER_ID, InventoryVariables.HAMBURGER }
-            };
-        }
-    }
 }
---
+++ Grocer/InventoryVariables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grocer
+{
+    public static class InventoryVariables
+    {
+        public static string HAMBURGER_ID = "hamburger";
+        public static decimal HAMBURGER_PRICE = 4.99m;
+        public static decimal HAMBURGER_MARKDOWN = 0;
+
+        public static InventoryItem HAMBURGER = new InventoryItem
+        {
+            Name = HAMBURGER_ID,
+            Price = HAMBURGER_PRICE,
+            MarkDown = HAMBURGER_MARKDOWN
+        };
+
+        public static void ResetInventory()
+        {
+            ClientInventory.InventoryDictionary = new Dictionary<string, InventoryItem>
+            {
+                {InventoryVariables.HAMBURGER_ID, InventoryVariables.HAMBURGER }
+            };
+        }
+    }
+}
