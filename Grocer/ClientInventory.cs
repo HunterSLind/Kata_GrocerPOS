bab5447e39f5d3c49c92970ddab4576e437ccd57
HLind <hunterslind@gmail.com>
2019-03-06 17:37:51 -0600

    Deleted the InventoryDictionary autofill contents. These items would be populated via a database. Moreover, the tests populate it when they're initialized.

--- Grocer/ClientInventory.cs
+++ Grocer/ClientInventory.cs
@@ -6,11 +6,6 @@ namespace Grocer
 {
     public static class ClientInventory
     {
-        public static Dictionary<string, InventoryItem> InventoryDictionary = new Dictionary<string, InventoryItem>
-        {
-            {InventoryVariables.HAMBURGER_ID, InventoryVariables.HAMBURGER },
-            {InventoryVariables.APPLE_ID, InventoryVariables.APPLE },
-            {InventoryVariables.BANANA_ID, InventoryVariables.BANANA }
-        };
+        public static Dictionary<string, InventoryItem> InventoryDictionary = new Dictionary<string, InventoryItem>();
     }
 }
